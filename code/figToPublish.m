9ea5f21d72ea112fcbe8a9cc99e52a0f82ba8fa7
Vedant Chandra <me@vedantchandra.com>
2019-02-21 01:44:44 -0500

    bug fixes

---
+++ code/figToPublish.m
@@ -0,0 +1,20 @@
+clear; close all;
+origin = pwd;
+
+[file,filepath] = uigetfile('*.fig','multiselect','on');
+
+%%
+
+cd(filepath)
+mkdir('png')
+mkdir('pdf')
+for i = 1:length(file)
+    
+   figure = openfig(file{i},'invisible');
+   filename = file{i};
+   filename = filename(1:end-3);
+   saveas(figure,strcat('png/',filename,'.png'));
+   saveas(figure,strcat('pdf/',filename,'.pdf'));
+   
+end
+
