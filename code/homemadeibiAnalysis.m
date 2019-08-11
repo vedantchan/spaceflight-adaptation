a2c8c2fa3d61e872b4f37521cac13f7b4334cbdf
Stang <stang29@jhu.edu>
2019-08-10 20:32:59 -0400

    skylab homemadeibi2

--- code/homemadeibiAnalysis.m
+++ code/homemadeibiAnalysis.m
@@ -1,13 +1,16 @@
 % 8/1/19: homemade IBI analysis
 
 %8/10/19: temp mod for skylab data
+%8/10/19: changed to low pass, high pass, threshold function (homemadeibi2)
+% >> param = 26,1
+
 clear all;
 close;
 
 %choose multiple subject folders in raw
 subjects = uipickfiles('filterspec','/Users/SYT/Documents/GitHub/spaceflight-adaptation/data');
 % store into a separate folder called...homemadeibi-[inserttrialname]
-mkdir('/Users/SYT/Documents/GitHub/spaceflight-adaptation/data/homemadeibi')
+mkdir('/Users/SYT/Documents/GitHub/spaceflight-adaptation/data/homemadeibi2')
 for subjcount = 1:length(subjects)
    %bvpepoch = dir(fullfile(subjects{subjcount},'EmpaticaSplit','*BVP*.csv'));
    bvpepoch = dir(fullfile(subjects{subjcount},'*BVP*.csv'));
@@ -26,26 +29,31 @@ for subjcount = 1:length(subjects)
     for file = 1:5
         %data = importdata(fullfile(subjects{subjcount},'EmpaticaSplit',srtd{file}));
         data = importdata(fullfile(subjects{subjcount},srtd{file}));
-        [ibi,pks,lc] = homemadeibi(data);
+        [ibi,pks,lc] = homemadeibi2(data);
         x = 1:length(data);
-        plot(x, data,lc,pks,'*')
-        title(['Peak Locations on BVP for IBI calculations:' 'subj' num2str(subjcount)])
-        subjfold = ['/Users/SYT/Documents/GitHub/spaceflight-adaptation/data/homemadeibi/subj' num2str(subjcount)];
+        lp = lowpass(data,5,64); %for ~170bpm (looser lp)
+        hp = highpass(lp,.05); %for 40bpm ???? <<just randomly chose a number, not sure the logic
+        ind = find(hp > 100);
+        hp(ind) = 0;
+        plot(x, hp,lc,pks,'*')
+        title(['Peak Locations on BVP for IBI calculations:' 'subj1969-' num2str(subjcount) '-E' num2str(file)])
+        subjfold = ['/Users/SYT/Documents/GitHub/spaceflight-adaptation/data/homemadeibi/subj1969-' num2str(subjcount)];
         mkdir(subjfold)
         saveas(gca, fullfile(subjfold,['peakloc' '_E' num2str(file) '.fig']))
         tot{file} = ibi;
         hold off
         %plot(ibi);
     end
-    dlmwrite(fullfile(subjfold,['homemadeibi_UP1.csv']),tot{1})
-    dlmwrite(fullfile(subjfold,['homemadeibi_UP2.csv']),tot{2})
-    dlmwrite(fullfile(subjfold,['homemadeibi' '_P1.csv']),tot{3})
-    dlmwrite(fullfile(subjfold,['homemadeibi_P2.csv']),tot{4})
-    dlmwrite(fullfile(subjfold,['homemadeibi_REC.csv']),tot{5})
+    dlmwrite(fullfile(subjfold,['homemadeibi2_UP1.csv']),tot{1})
+    dlmwrite(fullfile(subjfold,['homemadeibi2_UP2.csv']),tot{2})
+    dlmwrite(fullfile(subjfold,['homemadeibi2' '_P1.csv']),tot{3})
+    dlmwrite(fullfile(subjfold,['homemadeibi2_P2.csv']),tot{4})
+    dlmwrite(fullfile(subjfold,['homemadeibi2_REC.csv']),tot{5})
 
 
 end
 
+%ibi function 1, only with findpeaks function
 function [ibi,pks,lc] = homemadeibi(bvp)
 %find peaks past a certain threshold > 'minpeakdistance' = .4 sec (for 64Hz
 %>> https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5028198/ .4 sec
@@ -56,8 +64,22 @@ function [ibi,pks,lc] = homemadeibi(bvp)
 %use diff on peaks vector to find ibi
 %store ibi
 
+%changed to reduce peak distance and height for skylab
 [pks,lc] = findpeaks(bvp,'minpeakdistance',10,'minpeakheight',1);
 ibi = diff(lc); %this would be in... 1/64 sec? need to convert?
 
 end
 
+%with low pass, high pass, threshold, 
+function [ibi2,pks2,lc2] = homemadeibi2(bvp)
+lp = lowpass(bvp,5,64); %for ~170bpm (looser lp)
+hp = highpass(lp,.05); %for 40bpm ???? <<just randomly chose a number, not sure the logic
+ind = find(hp > 100);
+hp(ind) = 0;
+
+%[pks,lc] = findpeaks(hp,'minpeakdistance',1,'minpeakheight',1);
+[pks2,lc2] = findpeaks(hp,'minpeakdistance',26,'minpeakheight',1);
+
+ibi2 = diff(lc2); %this would be in... 1/64 sec? need to convert?
+
+end
